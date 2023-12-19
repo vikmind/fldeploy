b8817f39e23ada5c0c32dcf452d988f7d50adc15
Viktor Marchenko <viktor.marchenko@gmail.com>
2023-12-19 23:10:42 +0000

    setting url

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times 😱:',
+              'You have pushed the button this many times 😱😱😱:',
             ),
             Text(
               '$_counter',
