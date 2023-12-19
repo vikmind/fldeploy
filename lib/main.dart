e52d2808ea93d97eed4a3441d366ef4106a803e0
Viktor Marchenko <viktor.marchenko@gmail.com>
2023-12-19 23:43:52 +0000

    new PR

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'This is the change from Pull request',
+              'New PR',
             ),
             Text(
               '$_counter',
